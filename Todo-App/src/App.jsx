2331dbe3720627441c3a44b7cfe63d67fec47e7b
abrar-syed <abrarsd2024@gmail.com>
2025-04-22 16:25:21 -0500

    Todo-App changes

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react"
 import classes from "../src/style.module.css"
 import TodoItem from "./components/todo-item"; 
+import TodoDetails from "./components/todo-details";
+import { Skeleton } from "@mui/material";
 
 function App() {
 
   const [loading, setLoading] = useState(false);
   const [todoList, setTodoList] = useState([]);
   const [errorMsg, setErrorMsg] = useState(null)
+  const [todoDetails, setTodoDetails] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
 
   async function fetchListofTodos(){
     try{
@@ -24,29 +28,61 @@ function App() {
 
       }
       
-    }catch(err){
+    }
+    catch(err){
       console.log(err);
       setErrorMsg("Some error occured while fetching todos")
     }
 
   }
 
+  async function fetchDetailsOfCurrentTodo(currentTodoId){
+    
+    // console.log(currentTodoId);
+    
+    try{
+
+      const apiResponse = await fetch (`https://dummyjson.com/todos/${currentTodoId}`)
+      const details = await apiResponse.json();
+      console.log(details);
+      
+      if(details){
+        setTodoDetails(details);
+        setOpenDialog(true)
+      }
+      else{
+        setTodoDetails(null);
+        setOpenDialog(false);
+      }
+    }catch (err){
+      console.log(err);
+      
+    }
+  }
 
   useEffect(()=>{
     fetchListofTodos();
   },[])
 
+  if(loading) return <Skeleton variant="rectangular" width={650} height={650}/>
+
   return (
     <div className={classes.mainWrapper}>
       <h1 className={classes.headerTitle}>Simple Todo App Using Material UI</h1>
-      <div>
+      <div className={classes.todoListWrapper}>
         {
           todoList && todoList.length > 0
-          ? todoList.map((todoItem) => <TodoItem todo={todoItem}/>)
+          ? todoList.map((todoItem) => <TodoItem fetchDetailsOfCurrentTodo={fetchDetailsOfCurrentTodo} todo={todoItem}/>)
           : null
 
         }
       </div>
+      <TodoDetails 
+      openDialog={openDialog} 
+      todoDetails={todoDetails}
+      setOpenDialog={setOpenDialog}
+      setTodoDetails={setTodoDetails}
+      />
     </div>
       
   )
--- Todo-App/src/components/todo-item/index.jsx
+++ Todo-App/src/components/todo-item/index.jsx
@@ -1,12 +1,34 @@
-import {Card, CardContent, Typography} from "@mui/material";
+import {Button, Card, CardActions, CardContent, Typography} from "@mui/material";
 
-function TodoItem({todo}){
-    return <Card>
+function TodoItem({todo, fetchDetailsOfCurrentTodo}){
+    return <Card sx={{
+        maxWidth: 350,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    }}>
         <CardContent>
             <Typography variant="h5" color={'text.secondary'}>
                 {todo.todo}
             </Typography>
         </CardContent>
+        <CardActions>
+            <Button 
+            onClick={()=> fetchDetailsOfCurrentTodo(todo.id)}
+            sx ={{
+                backgroundColor:'#000000',
+                color:'#fff',
+                opacity: '0.75',
+                '&:hover' : {
+                    backgroundColor: '#000000',
+                    color:'#fff',
+                    opacity:'1'
+                }
+                
+            }}>
+                Details
+            </Button>
+        </CardActions>
 
     </Card>
 }
